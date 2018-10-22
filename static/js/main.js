a250bae6655c1755286da3259a4cc36dc2843fce
Darius <darius@nabaka.com>
2018-10-22 21:50:22 +0300

    Original tempalte

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,43 @@
-// Js goes here
+$(document).ready(function () {
+    appLesson1.draw();
+});
+
+appLesson1 = {
+    draw: function() {
+        $('#page-frame').removeClass('hidden');
+
+        appLesson1.drawHeader();
+        appLesson1.drawSlogan();
+        appLesson1.drawFooter();
+        appLesson1.initNews();
+    },
+    drawHeader: function() {
+        var headerTemplate = Handlebars.compile($("#ht-header").html());
+        var generatedTemplate = headerTemplate();
+        $('.header').append(generatedTemplate);
+    },
+    drawSlogan: function() {
+        var sloganTemplate = Handlebars.compile($("#ht-slogan").html());
+        var generatedTemplate = sloganTemplate();
+        $('.slogan').append(generatedTemplate);
+    },
+    initNews: function() {
+        $.ajax({
+            type: "GET",
+            dataType:'json',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            success: appLesson1.drawNews
+        });
+    },
+    drawNews: function (data) {
+        var newsTemplate = Handlebars.compile($("#ht-new-list").html());
+        var generatedTemplate = newsTemplate({'list': data});
+        $('.area-new-list').append(generatedTemplate);
+        console.log(data);
+    },
+    drawFooter: function() {
+        var tmpl = Handlebars.compile($("#ht-footer").html());
+        var genTmpl = tmpl();
+        $('.footer').append(genTmpl);
+    }
+};
